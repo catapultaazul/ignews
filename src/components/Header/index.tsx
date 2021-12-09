723504453d12195259c66a0db252ccbf581740ff
lucas <lucasguilheme.31@gmail.com>
2021-12-09 11:41:58 -0300

    Completed 'Front-end JAMStack: Working with CMS'

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 import { SignInButton } from "../SigInButton";
 import styles from "./styles.module.scss";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="#">
-            Home
-          </a>
-          <a href="#">Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
